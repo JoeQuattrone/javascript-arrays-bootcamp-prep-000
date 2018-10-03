b99dbf80c4869fa2d01532aadc9add89be140678
Joe Quattrone <joequattrone5@gmail.com>
2018-10-03 19:25:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,9 +3,6 @@ console.log(chocolateBars)
 
 
 
-function addElementToBeginningOfArray(array, element) {
-   return array.unshift(element)
-}
 
 function addElementToBeginningOfArray(array, element) {
    return [element, ...array]
