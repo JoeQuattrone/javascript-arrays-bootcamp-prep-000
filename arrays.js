1efff4038ad06899e759430b3e0afe3f9370ea8c
Joe Quattrone <joequattrone5@gmail.com>
2018-10-03 19:55:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,6 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray() {
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift(element)
 }
