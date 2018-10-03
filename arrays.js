fb8c1ce54445367523762c1faf8dd1fd68a4cf1b
Joe Quattrone <joequattrone5@gmail.com>
2018-10-03 20:03:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,6 @@ function destructivelyRemoveElementFromEndOfArray(array, element) {
 }
 
 function removeElementFromEndOfArray(array, element) {
-  return array.slice(-1)
+   array.slice(-1)
+   return array
 }
