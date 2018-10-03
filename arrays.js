e54d23bac7584db4b819d32a4c37fd5cf722974a
Joe Quattrone <joequattrone5@gmail.com>
2018-10-03 19:23:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 console.log(chocolateBars)
 
-
+var array = [1]
+var element = 'foo'
 
 function addElementToBeginningOfArray(array, element) {
    return array.unshift(element)
